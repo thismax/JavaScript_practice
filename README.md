6f002055a9d21889d2849fbd5038c1895a42753c
Max Jacobs <Max@Maxs-MacBook-Pro.local>
2017-06-22 10:13:42 -0700

    updates

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Read Me
 
-### This is a read me.  Feel free to read me.
-
 oddEvenOutlier
 ---------------------
+
+Takes a string of even and odd numbers seperated by single spaces and returns the location of either sole even or sole odd number.
+
+```javascript
+detectOutlierValue("1 3 5 6 7") //=> 4, since the fourth number is even while the rest are odd.
+```
--- oddEvenOutlier.js
+++ oddEvenOutlier.js
@@ -1,13 +1,3 @@
-/*
-Given a string of even and odd numbers, find which is the sole even number or the sole odd number.
-  
-The return value should be 1-indexed, not 0-indexed.
-
-Examples :
-detectOutlierValue("2 4 7 8 10"); // => 3 - Third number is odd, while the rest of the numbers are even
-detectOutlierValue("1 10 1 1");  //=> 2 - Second number is even, while the rest of the numbers are odd
-*/
-
 function detectOutlierValue(number_string) {
   
   var numbers = number_string.split(" ");
